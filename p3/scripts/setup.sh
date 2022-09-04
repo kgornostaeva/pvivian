bc08c7b3b4fac34880f537bf39df4294c8a83772
kate-eversunny <17optimistic@gmail.com>
2022-09-04 22:48:52 +0300

    Initial commit

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -1,67 +1,50 @@
-vagrant up
-vagrant ssh pvivianS
+#!/bin/bash
 
-sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-* 
-sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-* 
+echo -e "\033[1;33mLoading and updating OS\033[0m\n"
+sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 sudo yum update -y
-sudo yum -y install net-tools
+yum install net-tools -y
+export PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin"
+swapoff -a
 
-
-#k3d
+echo -e "\033[1;33mLoading k3d\033[0m\n"
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
-#docker
+echo -e "\033[1;33mLoading docker\033[0m\n"
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
-sudo usermod -aG docker $USER
-newgrp docker
 
-#kubectl
+echo -e "\033[1;33mLoading kubectl\033[0m\n"
 sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-sudo chmod +x kubectl
+sudo chmod +x /usr/local/bin/kubectl
 
-#argocd
+echo -e "\033[1;33mLoading argocd\033[0m\n"
 sudo curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
 sudo chmod +x /usr/local/bin/argocd
 
-#start
-
+echo -e "\033[1;33mStart docker\033[0m\n"
+sudo usermod -aG docker vagrant
+newgrp docker
 sudo systemctl start docker
 sudo systemctl enable docker
 
-k3d cluster create mycluster
+echo -e "\033[1;33mCreate cluster\033[0m\n"
+k3d cluster create iotcluster
+k3d kubeconfig merge iotcluster --kubeconfig-merge-default
 
+echo -e "\033[1;33mSetup argocd\033[0m\n"
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-#sudo reboot
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+kubectl -n argocd patch secret argocd-secret \
+  -p '{"stringData": {
+    "admin.password": "$2a$12$UCVHENi56FC3x2div0FC1.EJkw.QHxNq4aHTTeYB8Lxks9XiDYXDS",
+    "admin.passwordMtime": "'$(date +%FT%T%Z)'"
+  }}'
 
-cat > ingress.yaml << EOF
-apiVersion: networking.k8s.io/v1
-kind: Ingress
-metadata:
-  name: argocd-ingress
-  labels:
-      app: argocd
-  annotations:
-    nginx.ingress.kubernetes.io/rewrite-target: /$1
-    nginx.ingress.kubernetes.io/ssl-redirect: "false"
-spec:
-  rules:
-    - http:
-        paths:
-          - path: /argocd
-            pathType: Prefix
-            backend:
-              service:
-                name: argocd-server
-                port:
-                  number: 80
-EOFkubectl describe pod -n dev
-
-#kubectl port-forward svc/argocd-server -n argocd 8080:443
-# kubectl port-forward svc/argocd-iot-service -n dev 8888
-
-
-kubectl apply -f confs/ingress.yaml -n argocd
+kubectl create namespace dev
+kubectl apply -f /vagrant/confs/project.yaml -n argocd
+kubectl apply -f /vagrant/confs/application.yaml -n argocd

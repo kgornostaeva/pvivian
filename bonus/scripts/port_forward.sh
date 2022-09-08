537ac06ec621b2d84814e5d9d3d5d7f099ed2d44
kate-eversunny <17optimistic@gmail.com>
2022-09-08 21:52:03 +0300

    Update configurations

--- bonus/scripts/port_forward.sh
+++ bonus/scripts/port_forward.sh
@@ -4,5 +4,5 @@ k3d kubeconfig merge gitlab --kubeconfig-merge-default
 conditions="-n argocd -l app.kubernetes.io/name=argocd-server --timeout=10m"
 kubectl wait --for=condition=available deployment $conditions
 kubectl wait --for=condition=ready pod $conditions
-kubectl port-forward svc/argocd-server -n argocd 8080:80 --address=0.0.0.0 &
+kubectl port-forward svc/argocd-server -n argocd 9999:80 --address=0.0.0.0 &
 kubectl port-forward svc/argocd-iot-service -n dev 8888:8888 --address=0.0.0.0 &
